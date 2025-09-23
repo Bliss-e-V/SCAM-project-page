c5d98f268489f8ef94aef0508d61e69f7ca2d474
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2025-09-23 18:52:29 +0200

    better image quality

--- data_images_generator.py
+++ data_images_generator.py
@@ -1,16 +1,14 @@
 # Download SCAM dataset and generate image folders with optimized images for the project page
 
-from datasets import load_dataset, Image as HfImage
+from datasets import load_dataset
 from pathlib import Path
-from PIL import Image
+from tqdm import tqdm
 
-dataset = load_dataset("BLISS-e-V/SCAM", split="train").cast_column("image", HfImage(decode=False))
+dataset = load_dataset("BLISS-e-V/SCAM", split="train")
 
-for elem in dataset:
+for elem in tqdm(dataset):
     ds_type = elem["type"]
-    input_path = elem["image"]['path']
-    name = Path(input_path).stem
-    img = Image.open(input_path).convert("RGB").resize((256, 256))
-    output_path = Path(f"data_images/{ds_type}/{name}.webp")
+    img = elem["image"].convert("RGB").resize((384, 384))
+    output_path = Path(f"data_images/{ds_type}/{elem['id']}.webp")
     output_path.parent.mkdir(parents=True, exist_ok=True)
-    img.save(output_path, format="webp", quality=50)
+    img.save(output_path, format="webp", quality=70)
